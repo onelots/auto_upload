c906bbc326458a7bd640075bcf559a9f534f799f
Onelots <onelots@lineageos.org>
2026-06-30 13:26:25 +0200

    upload: Upload files through rclone
    Signed-off-by: Onelots <onelots@lineageos.org>

--- upload.py
+++ upload.py
@@ -3,9 +3,15 @@
 import os
 import sys
 import configparser
+import subprocess
+import re
 
 rclone_conf = os.path.expanduser("~/.config/rclone/rclone.conf")
 devices_conf = "devices.conf"
+config = configparser.ConfigParser()
+base_out = "out/target/product/"
+pwd = os.getenv("PWD")
+
 
 def check_conf():
     if not os.path.exists(rclone_conf):
@@ -18,12 +24,61 @@ def check_conf():
         sys.exit(1)
     print("Device configuration found, proceed.")
 
+
+def get_android_ver(device):
+    rom = config[device]["rom"]
+    if "lineage" in rom:
+        if "lineage-18" in pwd:
+            aver = 11
+        if "lineage-19" in pwd:
+            aver = 12
+        if "lineage-20" in pwd:
+            aver = 13
+        if "lineage-21" in pwd:
+            aver = 14
+        if "lineage-22" in pwd:
+            aver = 15
+        if "lineage-23" in pwd:
+            aver = 16
+        if "lineage-24" in pwd:
+            aver = 17
+    if "evolutionx" in rom:
+        if "10" in pwd:
+            aver = 15
+        if "11" in pwd:
+            aver = 16
+        if "12" in pwd:
+            aver = 17
+    
+    return int(aver)
+
+
+def upload_device(device):
+    rom = config[device]["rom"]
+    base_path = base_out + device + "/"
+    android_version = get_android_ver(device)
+    zips = [f for f in os.listdir(base_path) if f.endswith(".zip") and "ota-eng" not in f]
+    rom_zip = max(zips, key=lambda f: os.path.getmtime(f"{base_path}/{f}"))
+    major_version = re.findall(r'\d+\.\d+', rom_zip)[-1]
+    build_date = re.search(r'\d{8}', rom_zip).group()
+    upload_path=f"{rom}/{device}/{android_version}/{major_version}/{build_date}"
+    install_images = config[device]["install_images"]
+    install_images = [img.strip() for img in install_images.split(",")]
+    
+    install_images.append(rom_zip)
+
+    # Upload stuff
+    for image in install_images:
+        if not image.endswith(".zip"):
+            image = f"{image}.img"
+        subprocess.run(["rclone", "copy", f"{base_path}/{image}", f"cloudflare-onelots:{upload_path}", "-P"])
+
+
 def read_device_config_file(file_path, built):
-    config = configparser.ConfigParser()
     config.read(file_path)
     devices = config.sections()
     if built in devices:
-        upload_device()
+        upload_device(built)
     else:
         print(f"Device '{built}' not found in config, exiting...")
         sys.exit(1)
